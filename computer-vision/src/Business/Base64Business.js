05beaf2dc7cec79c75048d3469fc59fe8b9f5dda
fernando.lima <fol2111@gmail.com>
2017-08-10 04:22:59 -0300

    tests with base64

--- computer-vision/src/Business/Base64Business.js
+++ computer-vision/src/Business/Base64Business.js
@@ -1,4 +1,8 @@
+const fs = require('fs');
+const Readable = require('stream').Readable;
+
 const base64 = require('base-64');
+const base64Stream = require('base64-stream');
 const utf8 = require('utf8');
 
 
@@ -9,6 +13,14 @@ class Base64Business{
             return base64.encode(utf8.encode(data));
         }
         return base64.encode(data);
+    }
+
+    encodeToStream(data){
+        let readable = new Readable();
+        readable._read = () => {}
+        readable.push(data);
+        readable.push(null);
+        return readable;
     }
 
     decodeToBytes(data){
